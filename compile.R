8d052816ce38d0b143b5882e4e894d668f8f52c0
buchanankerswell <buck.kerswell@gmail.com>
2020-12-08 22:39:24 -0700

    cleanup directories

---
+++ compile.R
@@ -0,0 +1,120 @@
+rm(list = ls())
+source('functions.R')
+
+# Projections
+# WGS84 Pseudo-Mercator [Google Maps, OpenStreetMap, Bing, ArcGIS, ESRI]
+proj <- 3857
+proj4 <- '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs'
+
+# WGS84
+proj.wgs <- 4326
+proj4.wgs <- '+proj=longlat +datum=WGS84 +no_defs'
+
+# Robinson
+proj4.robin <- '+proj=robin +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m no_defs'
+proj4.robin.pacific <- '+proj=robin +lon_0=180 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m no_defs'
+
+# Graticules 30 degree
+shp.grats30 <- st_read('data/grats30/', quiet = T) %>% st_transform(proj4.robin.pacific)
+
+# Country Boundaries
+# Create sliver around zero degree long
+shp.zero.sliv <- st_polygon(x = list(rbind(c(-0.0001, 90), c(0, 90), c(0, -90), c(-0.0001, -90), c(-0.0001, 90)))) %>%
+  st_sfc() %>%
+  st_set_crs(proj.wgs)
+# Trim the countries boundaries overlapping at zero degree long and reproject
+shp.world.robin.pacific <- rnaturalearth::ne_countries(returnclass = 'sf') %>%
+  st_difference(shp.zero.sliv) %>%
+  tibble::as_tibble() %>%
+  st_as_sf() %>% st_transform(proj4.robin.pacific)
+shp.world.robin.pacific10 <- rnaturalearth::ne_countries(returnclass = 'sf', scale = 10) %>%
+  st_difference(shp.zero.sliv) %>%
+  tibble::as_tibble() %>%
+  st_as_sf() %>% st_transform(proj4.robin.pacific)
+
+
+# Read syracuse et al 2006 Segments
+# seg.names <- list.files('./sa2006/gmts/') %>% purrr::map_chr(~ gsub('.contours.gmt', '', .x))
+seg.names <- c('Alaska Aleutians', 'Andes', 'Central America', 'Kamchatka Marianas', 'Kyushu Ryukyu', 'Lesser Antilles', 'N. Philippines', 'New Britain Solomon', 'S. Philippines', 'Scotia', 'Sumatra Banda Sea', 'Tonga New Zealand', 'Vanuatu')
+files <- list.files('data/sa2006/gmts', full.names = TRUE)
+
+# Contours Robinson pacific centered
+shp.sa.countours.robin.pacific <- read_wrap_latlong(files, seg.names, proj4.robin.pacific)
+shp.sa.segs.robin.pacific <- shp.sa.countours.robin.pacific %>% group_by(segment) %>% filter(row_number() == 1)
+shp.sa.segs.robin.pacific.buffer <- shp.sa.segs.robin.pacific %>% st_buffer(500000, endCapStyle = 'ROUND') %>% st_wrap_dateline(options = c("WRAPDATELINE=YES"))
+
+# Make boxes around segments
+box.segs.all <- st_bbox(shp.sa.segs.robin.pacific.buffer) %>% bbox_widen(proj4.robin.pacific, c('left' = 0, 'right' = 0, 'top' = 0, 'bottom' = 0))
+box.segs.all.wide <- st_bbox(shp.sa.segs.robin.pacific.buffer) %>% bbox_widen(proj4.robin.pacific, c('left' = 0.02, 'right' = 0.01, 'top' = 0.008, 'bottom' = 0.008))
+box.segs.bbox <- shp.sa.segs.robin.pacific.buffer %>% tidyr::nest() %>% purrr::pmap(~st_bbox(..2)) %>% purrr::set_names(unique(shp.sa.segs.robin.pacific.buffer$segment))
+box.segs <- box.segs.bbox  %>% purrr::map(~bbox_widen(.x, crs = proj4.robin.pacific, borders = c('left' = 0, 'right' = 0, 'top' = 0, 'bottom' = 0)))
+box.segs.wide <- box.segs.bbox %>% purrr::map(~bbox_widen(.x, crs = proj4.robin.pacific, borders = c('left' = 0.05, 'right' = 0.05, 'top' = 0.05, 'bottom' = 0.05)))
+
+# Draw box to stuff pacific labels into
+box.lab <- st_bbox(shp.sa.segs.robin.pacific) %>% bbox_widen(proj4.robin.pacific, borders = c('left' = -0.3, 'right' = -0.25, 'top' = -0.12, 'bottom' = -0.40))
+
+# Read global heat flow database (IHFC 2010), turn into tibble,
+# remove useless columns & filter heat flow between 0 and 200, then crop to buffers
+shp.hf <- st_read('data/hf/', coords = c(3,2), crs = proj.wgs, quiet = T) %>%
+  st_transform(proj4.robin.pacific) %>%
+  tibble::as_tibble() %>% st_as_sf() %>%
+  select(-Data_Numbe, -Codes, -Year_of_Pu, -Comments, 
+         -F21, -F22, -F23, -F24, -F25, -F26, -ben, -europe) %>%
+  filter(Heat_Flow <= 200 & Heat_Flow >= 0) %>%
+  mutate('No__Temps' = as.numeric(No__Temps), 'No_Heat_Pr' = as.numeric(No_Heat_Pr), 'Heat_Prod_' = as.numeric(Heat_Prod_)) %>%
+  st_join(shp.sa.segs.robin.pacific.buffer, left = F)
+
+# Read syracuse et al 2006 volcanoes
+# Header
+h.volc <- readr::read_table('data/sa2006/volcanoes.txt', n_max = 1)
+# Read table without header
+volc <- readr::read_table('data/sa2006/volcanoes.txt', skip = 2)
+# Combine
+names(volc) <- names(h.volc)
+# Tidy
+volcano <- volc %>%
+  mutate(
+    'ArcName' = as.factor(ArcName),
+    'Age' = as.numeric(Age),
+    'Phi/100' = as.numeric(`Phi/100`)) %>%
+  filter(!is.na(H))
+
+# Header
+h.volc.no.spread <- readr::read_table('data/sa2006/volcanoes_nospread.txt', n_max = 1)
+# Read table without header
+volc.no.spread <- readr::read_table('data/sa2006/volcanoes_nospread.txt', skip = 2)
+# Combine
+names(volc.no.spread) <- names(h.volc.no.spread)
+# Tidy
+volcano.no.spread <- volc.no.spread %>%
+  mutate(
+    'ArcName' = as.factor(ArcName),
+    'Age' = as.numeric(Age),
+    'H' = as.numeric(H),
+    'Dip' = as.numeric(Dip),
+    'DesRat' = as.numeric(DesRat),
+    'Phi/100' = as.numeric(`Phi/100`)
+  ) %>%
+  filter(!is.na(H))
+
+# Change to simple features object and project
+shp.volc <- volcano %>%
+  st_as_sf(coords = c('Lon', 'Lat')) %>%
+  st_set_crs(proj.wgs) %>%
+  st_transform(proj4.robin.pacific)
+shp.volc.no.spread <- volcano.no.spread %>%
+  mutate(
+    'ArcName' = as.factor(ArcName),
+    'H' = as.numeric(H),
+    'Dip' = as.numeric(Dip),
+    'DesRat' = as.numeric(DesRat),
+    '`Phi/100`' = as.numeric(`Phi/100`),
+  ) %>% 
+  filter(!is.na(H)) %>%
+  st_as_sf(coords = c('Lon', 'Lat')) %>%
+  st_set_crs(proj.wgs) %>%
+  st_transform(proj4.robin.pacific)
+
+rm(list = lsf.str())
+rm(files, h.volc, h.volc.no.spread)
+save.image('data/hf.RData')
---
+++ functions.R
@@ -0,0 +1,207 @@
+if(!identical(getOption("bitmapType"), "cairo") && isTRUE(capabilities()[["cairo"]])){
+  options(bitmapType = "cairo")
+}
+library(leaflet)
+library(ggsflabel)
+library(ggplot2)
+library(dplyr)
+library(sf)
+library(ggmap)
+library(cowplot)
+library(gstat)
+library(sp)
+library(stars)
+library(patchwork)
+library(ggrepel)
+
+# Define a function to fix the bbox to be in EPSG:3857
+ggmap_bbox <- function(map) {
+  if (!inherits(map, "ggmap")) stop("map must be a ggmap object")
+  # Extract the bounding box (in lat/lon) from the ggmap to a numeric vector, 
+  # and set the names to what sf::st_bbox expects:
+  map_bbox <- setNames(unlist(attr(map, "bb")), 
+                       c("ymin", "xmin", "ymax", "xmax"))
+  
+  # Coonvert the bbox to an sf polygon, transform it to 3857, 
+  # and convert back to a bbox (convoluted, but it works)
+  bbox_3857 <- st_bbox(st_transform(st_as_sfc(st_bbox(map_bbox, crs = 4326)), 3857))
+  
+  # Overwrite the bbox of the ggmap object with the transformed coordinates 
+  attr(map, "bb")$ll.lat <- bbox_3857["ymin"]
+  attr(map, "bb")$ll.lon <- bbox_3857["xmin"]
+  attr(map, "bb")$ur.lat <- bbox_3857["ymax"]
+  attr(map, "bb")$ur.lon <- bbox_3857["xmax"]
+  map
+}
+
+# Draw a widened box from a st_bbox object
+bbox_widen <- function(bbox, crs, borders = c('left' = 0.5, 'right' = 0.5, 'top' = 0, 'bottom' = 0)) {
+  b <- bbox # current bounding box
+  xrange <- b$xmax - b$xmin # range of x values
+  yrange <- b$ymax - b$ymin # range of y values
+  b[1] <- b[1] - (borders['left'] * xrange) # xmin - left
+  b[3] <- b[3] + (borders['right'] * xrange) # xmax - right
+  b[2] <- b[2] - (borders['bottom'] * yrange) # ymin - bottom
+  b[4] <- b[4] + (borders['top'] * yrange) # ymax - top
+  box <- st_polygon(list(matrix(c(b$xmin, b$ymax, b$xmin, b$ymin, b$xmax, b$ymin, b$xmax, b$ymax, b$xmin, b$ymax), ncol = 2, byrow = TRUE))) %>%
+    st_sfc(crs = crs)
+  return(box)
+}
+
+# Add alpha to ggmap
+ggmap_alpha <- function(map, alpha = 0.5) {
+  b.at <- attributes(map)
+  n.map <- matrix(adjustcolor(map, alpha.f = alpha), nrow = nrow(map))
+  attributes(n.map) <- b.at
+  return(n.map)
+}
+
+# Get bbox of multiple sf objects
+bbox_stitch <- function(bbox, crs) {
+  b <- bind_rows(bbox)
+  box <- st_polygon(list(matrix(c(
+    min(b$xmin), max(b$ymax),
+    min(b$xmin), min(b$ymin),
+    max(b$xmax), min(b$ymin),
+    max(b$xmax), max(b$ymax),
+    min(b$xmin), max(b$ymax)),
+    ncol = 2, byrow = TRUE))) %>%
+    st_sfc(crs = crs)
+  return(box)
+}
+
+# Read gmt files, wrap the dateline to avoid plotting horizontal lines on map,
+# make into tibble, add segment names, transform projection, and bind into one sf object
+read_wrap_latlong <- function(files, filenames = NULL, crs) {
+  if(is.null(filenames)) {
+    shp <- purrr::map2(files, filenames, 
+                       ~ st_read(.x, crs = proj.wgs, quiet = TRUE) %>%
+                         st_wrap_dateline(options = c("WRAPDATELINE=YES","DATELINEOFFSET=180")) %>%
+                         st_transform(crs) %>%
+                         tibble::as_tibble() %>%
+                         tibble::add_column('segment' = .y) %>%
+                         group_by(segment) %>%
+                         st_as_sf()) %>%
+      bind_rows()
+  } else {
+    shp <- purrr::map2(files, filenames, 
+                       ~ st_read(.x, crs = proj.wgs, quiet = TRUE) %>%
+                         st_wrap_dateline(options = c("WRAPDATELINE=YES","DATELINEOFFSET=180")) %>%
+                         st_transform(crs) %>%
+                         tibble::as_tibble() %>%
+                         tibble::add_column('segment' = .y) %>%
+                         group_by(segment) %>%
+                         st_as_sf()) %>%
+      purrr::set_names(nm = filenames) %>%
+      bind_rows()
+  }
+  return(shp)
+}
+
+# Heat flow map plot
+plot_hf <- function(
+  grats = NULL,
+  world = NULL,
+  box = NULL,
+  box.col = 'cornflowerblue',
+  box.alpha = 0.15,
+  seg = NULL,
+  seg.contours = NULL,
+  seg.buffer = NULL,
+  seg.names = TRUE,
+  seg.label.size = 3,
+  seg.label.alpha = 0.7,
+  heatflow = NULL,
+  hf.size = 0.1,
+  viridis.pal = 'viridis',
+  volcano = NULL,
+  volc.size.range = c(1, 6),
+  volc.size = 0.5,
+  volc.alpha = 1,
+  volc.color = 'black',
+  box.labels = NULL,
+  box.limits = NULL,
+  legend.pos = 'right',
+  legend.just = 'left',
+  legend.size = unit(8, 'lines'),
+  plot.labs = NULL) {
+  if("patchwork" %in% (.packages())){
+    detach("package:patchwork", unload=TRUE) 
+  }
+  volcs <- volcano %>% bind_rows()
+  p <- ggplot()
+  if(!is.null(grats)) {p <- p + geom_sf(data = grats, size = 0.15)}
+  if(!is.null(world)) {p <- p + geom_sf(data = world, alpha = 0.8, color = NA)}
+  if(!is.null(box)) {p <- p + geom_sf(data = box, fill = 'cornflowerblue', alpha = 0.15, color = NA)}
+  if(!is.null(seg.contours)) {p <- p + geom_sf(data = seg.contours, size = 0.25, color = 'black')}
+  if(!is.null(seg)) {p <- p + geom_sf(data = seg, size = 1, color = 'black')}
+  if(!is.null(seg.buffer)) {p <- p + geom_sf(data = seg.buffer, fill = 'black', color = NA, alpha = 0.1)}
+  if(!is.null(heatflow)) {p <- p + geom_sf(data = heatflow, aes(color = Heat_Flow), size = hf.size)}
+  if(!is.null(volcano)) {p <- p + geom_sf(data = volcs, aes(size = H), shape = 2, color = volc.color, alpha = volc.alpha, stroke = volc.size)}
+  if(!is.null(seg) && seg.names) {p <- p + geom_sf_label_repel(data = seg, aes(label = segment), size = seg.label.size, alpha = seg.label.alpha, xlim = c(st_bbox(box.labels)$xmin, st_bbox(box.labels)$xmax), ylim = c(st_bbox(box.labels)$ymin, st_bbox(box.labels)$ymax))}
+  if(!is.null(box.limits)) {p <- p + coord_sf(crs = proj4.robin.pacific, xlim = c(st_bbox(box.limits)$xmin, st_bbox(box.limits)$xmax), ylim = c(st_bbox(box.limits)$ymin, st_bbox(box.limits)$ymax))}
+  p <- p + scale_color_viridis_c(option = viridis.pal) + scale_size_continuous(range = volc.size.range)
+  ifelse(!is.null(plot.labs), {p <- p + labs(title = plot.labs, caption = 'Data: Syracuse & Abers (2006); IHFC (2010)', y = '', x = '')}, {p <- p + labs(caption = 'Data: Syracuse & Abers (2006); IHFC (2010)', y = '', x = '')})
+  ifelse(legend.pos %in% c('bottom', 'top'), {p <- p + guides(color = guide_colorbar(title = bquote(mW/m^2), barwidth = legend.size, ticks = F))}, {p <- p + guides(color = guide_colorbar(title = bquote(mW/m^2), barlength = legend.size, ticks = F))})
+  p <- p + guides(size = guide_legend(override.aes = list(alpha = 1)))
+  p <- p + theme_map(font_size = 11) + 
+    theme(
+      axis.text = element_text(),
+      legend.justification = legend.just,
+      legend.position = legend.pos,
+      panel.border = element_blank(),
+      panel.grid = element_line(size = 0.25, color = rgb(0.1, 0.1, 0.1, 0.5)),
+      panel.background = element_blank(),
+      panel.ontop = TRUE,
+      plot.background = element_rect(fill = "transparent", color = NA)
+    )
+  if (legend.pos %in% c('bottom', 'top')) {p <- p + theme(legend.box.margin = margin(0, 8, 0, 0), legend.margin = margin(0, 8, 0, 8))}
+  if (any(grepl('*Andes.*', plot.labs) == TRUE)) {p <- p + theme(plot.title = element_text(hjust = 1))}
+  return(p)
+}
+
+# Kriging
+krg <- function(data, lags = 50, param, krg.shp, seg, contours, crs, ngrid = 3e5, grid.method = 'hexagonal', v.mod = 'Sph', plot = TRUE){
+  d <- data
+  cutoff <- max(st_distance(data))/5 %>% as.numeric()
+  class(cutoff) <- 'numeric'
+  b <- krg.shp
+  s <- b %>% st_sample(size = ngrid, grid.method)
+  v <- variogram(get(param)~1, locations = d, cutoff = cutoff, width = cutoff/lags)
+  f <- fit.variogram(v, vgm(NA, 'Sph', NA, NA))
+  v.m <- variogramLine(f, maxdist = max(v$dist))
+  k <- krige(formula = get(param)~1, locations = d, newdata = s) %>% select(-var1.var)
+  if(plot == TRUE){
+    p.h <- ggplot() +
+      geom_histogram(data = d, aes_string(x = param, y = '..density..'), alpha = 0.8, color = 'white') +
+      labs(x = param, y = 'Density') +
+      theme_bw()
+    p.v <- ggplot() +
+      geom_point(data = v, aes(x = dist/1000, y = gamma), size = 0.5) +
+      geom_path(data = v.m, aes(x = dist/1000, y = gamma)) +
+      labs(x = 'Lag (km)', y = 'Semivariance') +
+      coord_cartesian(ylim = c(0, NA)) +
+      theme_bw()
+    p <- ggplot() +
+      geom_sf(data = bbox_widen(st_bbox(b), crs = crs, c('left' = 0.1, 'right' = 0.1, 'top' = 0.1, 'bottom' = 0.1)), fill = 'cornflowerblue', alpha = 0.2, color = NA) +
+      geom_stars(data = st_rasterize(k) %>% st_crop(b)) +
+      geom_sf(data = d, aes_string(color = param), size = 0.5, show.legend = F) +
+      geom_sf(data = contours, color = 'white', alpha = 0.8, size = 0.3) +
+      geom_sf(data = seg, fill = NA, color = 'black', size = 1.25) +
+      labs(x = NULL, y = NULL) +
+      scale_color_viridis_c(option = 1) +
+      scale_fill_viridis_c(name = bquote(atop('Heat Flow', mW/m^2)), option = 1, na.value = 'transparent') +
+      theme_map(font_size = 11) + 
+      theme(
+        axis.text = element_text(),
+        panel.border = element_blank(),
+        panel.grid = element_line(size = 0.25, color = rgb(0.1, 0.1, 0.1, 0.5)),
+        panel.background = element_blank(),
+        panel.ontop = TRUE,
+        plot.background = element_rect(fill = "transparent", color = NA)
+      )
+    return(list(variogram = v, v.model = f, krg = k, hist = p.h, k.plot = p, v.plot = p.v))
+  } else {
+    return(list(variogram = v, v.model = f, krg = k))
+  }
+}
